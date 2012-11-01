d89d77f163865b96a9bf32049f2cc223d4fa7f64
Matthew Smith <smith_matthew@live.com>
2012-11-01 16:45:40 -0400

    Added the class files for the input event and the model change event

---
+++ src/courseProject/controller/InputEvent.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package courseProject.controller;
+
+/**
+ * @author Matthew Smith
+ * @version 01/11/2012
+ */
+public class InputEvent {
+
+	/**
+	 * 
+	 */
+	public InputEvent() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/courseProject/model/ModelChangeEvent.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package courseProject.model;
+
+/**
+ * @author Matthew Smith
+ * @version 01/11/2012
+ */
+public class ModelChangeEvent {
+
+	/**
+	 * 
+	 */
+	public ModelChangeEvent() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
